7bc73499f6f1b57d604e43999e3e9077eea142c6
Yuval Kamani <yuvalkamani@gmail.com>
2024-03-16 19:14:13 -0400

    added new background colours

--- portfolio-new/components/Experience.jsx
+++ portfolio-new/components/Experience.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-black">
-      <h1 className="text-4xl font-bold text-white">Experience</h1>
+    <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-white">
+      <h1 className="text-4xl font-bold text-black">Experience</h1>
     </div>
   );
 };
--- portfolio-new/components/Extras.jsx
+++ portfolio-new/components/Extras.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Extras = () => {
   return (
-    <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-black">
+    <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-[#ef8354]">
       <h1 className="text-4xl font-bold text-white">Extras</h1>
     </div>
   );
