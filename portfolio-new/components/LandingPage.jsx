adc7cfe92cbf0f8aa51719510d07a6b17e3fbc93
Yuval Kamani <yuvalkamani@gmail.com>
2024-03-16 19:50:14 -0400

    minor change

--- portfolio-new/components/LandingPage.jsx
+++ portfolio-new/components/LandingPage.jsx
@@ -4,12 +4,9 @@ const LandingPage = () => {
   return (
     <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-b from-[#000000] to-[#060314]">
       <h1 className="text-5xl font-bold text-white">Yuval Kamani</h1>
-      <div className="text-xl font-light text-white flex flex-col items-center justify-center">
-        <p className="text-decoration-line: line-through">
-          Software Engineer + Mathematician
-        </p>
-        <p>4th Year Computer Science and Maths Candidate</p>
-      </div>
+      <p className="text-xl font-light text-white">
+        Software Engineer + Mathematician
+      </p>
     </div>
   );
 };
