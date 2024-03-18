e6aa926279a3dc777d3717c3e8186df1f5bf3f97
Yuval Kamani <yuvalkamani@gmail.com>
2024-03-18 13:58:32 -0400

    landing page modifications and content in about me

--- portfolio-new/components/AboutMe.jsx
+++ portfolio-new/components/AboutMe.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-b from-[#060314] to-[#090F26]">
-      <h1 className="text-4xl font-bold text-white">About Me</h1>
+    <div className="snap-start flex flex-col items-center justify-center min-h-screen w-screen bg-gradient-to-b from-[#060314] to-[#090F26] text-white p-4">
+      <div className="w-full max-w-4xl">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-left">
+          About Me
+        </h1>
+        <p className="text-left mt-6 text-base md:text-lg lg:text-xl">
+          I'm Yuval, a software engineer fueled by coffee, code, and a drive to
+          create awesome things. Currently challenged everyday at UofT (can't
+          wait to graduate).
+        </p>
+      </div>
     </div>
   );
 };
--- portfolio-new/components/LandingPage.jsx
+++ portfolio-new/components/LandingPage.jsx
@@ -1,11 +1,15 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const LandingPage = () => {
   return (
     <div className="snap-start flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-b from-[#000000] to-[#060314]">
-      <h1 className="text-5xl font-bold text-white">Yuval Kamani</h1>
-      <p className="text-xl font-light text-white">
-        Software Engineer + Mathematician
+      <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-center text-white">
+        Yuval Kamani
+      </h1>
+      <p className="text-center mt-6 text-base md:text-lg lg:text-xl text-white">
+        Software Engineer & Mathematician
       </p>
     </div>
   );
