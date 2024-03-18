c1427f3f735d3f5e78229babc67be4d49d80fdce
Yuval Kamani <yuvalkamani@gmail.com>
2024-03-18 16:34:01 -0400

    landing page change

--- portfolio-new/components/LandingPage.jsx
+++ portfolio-new/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import { mainDiv } from "./CSSConstants";
 const LandingPage = () => {
   return (
     <div className={`${mainDiv} bg-gradient-to-b from-[#000000] to-[#060314]`}>
-      <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-center">
+      <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-center">
         Yuval Kamani
       </h1>
       <p className="text-center text-base md:text-lg lg:text-xl">
