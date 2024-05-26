07ed2c9ec3fa2472cfe7c2b560da38549bdd1a06
Yuval Kamani <yuvalkamani@gmail.com>
2024-05-26 16:05:37 -0400

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Yuval Kamani",
   description: "SE & Mathematician",
+  icons: { icon: "/yuval.png" },
 };
 
 export default function RootLayout({
